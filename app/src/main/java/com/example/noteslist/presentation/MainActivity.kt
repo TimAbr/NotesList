a6bc90325894eb4a3779594889c8935dc00f5ea5
Timur Abrashin <timurabr.work@gmail.com>
2026-03-06 15:33:22 +0300

    fix: move activity to presentation layer

--- app/src/main/java/com/example/noteslist/presentation/MainActivity.kt
+++ app/src/main/java/com/example/noteslist/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.noteslist
+package com.example.noteslist.presentation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.noteslist.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
