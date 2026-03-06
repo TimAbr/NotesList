d8a5c670bfdb7ce626c460b8406f943adc40180f
Timur Abrashin <timurabr.work@gmail.com>
2026-03-06 15:19:54 +0300

    feat: add activity and basic layout for it

--- app/src/main/java/com/example/noteslist/MainActivity.kt
+++ app/src/main/java/com/example/noteslist/MainActivity.kt
@@ -1,3 +1,11 @@
 package com.example.noteslist
 
-//тут будешь ваша активити
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
