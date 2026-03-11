6dd298f0e9dcd44573817fd0d89d9fadebd3e37d
Timur Abrashin <timurabr.work@gmail.com>
2026-03-11 17:55:28 +0300

    fix: now every NoteView created by NoteDelegate has OnClickListener and attributes from default style

--- app/src/main/java/com/example/noteslist/presentation/notes_list/recycler_view/delegates/NoteDelegate.kt
+++ app/src/main/java/com/example/noteslist/presentation/notes_list/recycler_view/delegates/NoteDelegate.kt
@@ -1,21 +1,30 @@
 package com.example.noteslist.presentation.notes_list.recycler_view.delegates
 
 import android.view.ViewGroup
+import androidx.appcompat.view.ContextThemeWrapper
 import androidx.recyclerview.widget.RecyclerView
+import com.example.noteslist.R
+import com.example.noteslist.domain.models.Note
 import com.example.noteslist.presentation.notes_list.recycler_view.NoteListItem
 import com.example.noteslist.presentation.notes_list.views.note.NoteView
 
-class NoteDelegate : NoteListItemDelegate {
+class NoteDelegate(
+    private val onNoteClick: (Note)-> Unit
+) : NoteListItemDelegate {
     override fun isForViewType(item: NoteListItem) = item is NoteListItem.SingleNote
 
     override fun onCreateViewHolder(parent: ViewGroup): RecyclerView.ViewHolder {
-        val noteView = NoteView(parent.context).apply {
+        val styledContext = ContextThemeWrapper(
+            parent.context,
+            R.style.NoteStyle
+        )
+        val noteView = NoteView(styledContext).apply {
             layoutParams = ViewGroup.LayoutParams(
                 ViewGroup.LayoutParams.MATCH_PARENT,
                 ViewGroup.LayoutParams.WRAP_CONTENT
             )
         }
-        return NoteViewHolder(noteView)
+        return NoteViewHolder(noteView, onNoteClick)
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, item: NoteListItem) {
@@ -23,10 +32,15 @@ class NoteDelegate : NoteListItemDelegate {
     }
 
     class NoteViewHolder(
-        private val noteView: NoteView
+        private val noteView: NoteView,
+        private val onNoteClick: (Note)-> Unit
     ) : RecyclerView.ViewHolder(noteView) {
         fun bind(item: NoteListItem.SingleNote) {
             noteView.data = item.note
+
+            noteView.setOnClickListener {
+                onNoteClick(item.note)
+            }
         }
     }
 }
