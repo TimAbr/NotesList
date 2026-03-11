58131fe9760e3a837b3b3793b74bfa0073521c4c
Timur Abrashin <timurabr.work@gmail.com>
2026-03-11 12:44:38 +0300

    feat: add example to MainActivity to show NoteView

--- app/src/main/java/com/example/noteslist/presentation/MainActivity.kt
+++ app/src/main/java/com/example/noteslist/presentation/MainActivity.kt
@@ -1,12 +1,29 @@
 package com.example.noteslist.presentation
 
 import android.os.Bundle
+import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 import com.example.noteslist.R
+import com.example.noteslist.presentation.notes_list.views.note.NoteView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        
+        val container = findViewById<LinearLayout>(
+            R.id.notesContainer
+        )
+        
+        for (i in 0 until container.childCount) {
+            val child = container.getChildAt(i)
+            if (child is NoteView) {
+                child.setOnClickListener {
+                    child.data = child.data.copy(
+                        isRead = !child.data.isRead
+                    )
+                }
+            }
+        }
     }
 }
